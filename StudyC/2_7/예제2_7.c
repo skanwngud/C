144f2fc42dfd5073ba1cde08b4ea1eaad82a2964
skanwngud <skanwngud@gmail.com>
2021-03-09 11:47:42 +0900

    commit

---
+++ StudyC/2_7/예제2_7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%c\n", 'A'); // 문자 상수
+	printf("%s\n", "A"); // 문자열 상수
+	printf("%c은 %s 입니다.", '1', "first"); // 문자와 문자열 상수를 같이 출력
+
+	return 0;
+}
+
+/* 문자는 character, c
+문자열은 string, s 로 표현한다.*/
