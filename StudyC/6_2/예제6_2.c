12951cc972f996f5f54509e70f322b2eaecd4552
skanwngud <skanwngud@gmail.com>
2021-03-27 18:53:30 +0900

    commit

--- StudyC/6_2/예제6_2.c
+++ StudyC/6_2/예제6_2.c
@@ -2,11 +2,11 @@
 
 int main(void)
 {
-	int a = 1;
-	int i;
+	int a = 1; // a 를 1 로 초기화
+	int i; // i 변수 초기화
 
-	for (i = 0; i < 3; i++)
-	{
+	for (i = 0; i < 3; i++) // i 가 0, 1, 2 일 때 반복
+	{                       // a 값은 2 (1 * 2), 4 (2 * 2), 8 (4 * 2) 이 된다
 		a = a * 2;
 	}
 	printf("a : %d\n", a);
