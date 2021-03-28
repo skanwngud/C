d81bfd50ef5ae9869e506abc8afa0a0e43c68e7a
skanwngud <skanwngud@gmail.com>
2021-03-28 12:19:24 +0900

    commit

---
+++ StudyC/확인문제6_2/6_2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 5; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (i == j)
+			{
+				printf("*");
+			}
+
+			else if (i + j == 4)
+			{
+				printf("*");
+			}
+
+			printf("\t");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
