1c3c30f995f6af30e7f339eacbda48d06b2cf6e7
skanwngud <skanwngud@gmail.com>
2021-06-08 19:14:54 +0900

    commit

--- StudyC/학점은행제_실습과제_2/assignment_1.c
+++ StudyC/학점은행제_실습과제_2/assignment_1.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
 
 int main(void) {
-	// °³ÀÎº° ½ÇÀû
+	// ï¿½ï¿½ï¿½Îºï¿½ ï¿½ï¿½ï¿½ï¿½
 	int kim[5] = { 34000, 56000, 23000, 78000, 191000 };
 	int lee[5] = { 23000, 65000, 18000, 94000, 200000 };
 	int park[5] = { 25000, 34000, 19000, 68000, 146000 };
 	int song[5] = { 18000, 52000, 23500, 73950, 167450 };
 	int choi[5] = { 25000, 38000, 26000, 64000, 153000 };
 
-	// ºÐ±âº° °³ÀÎ ½ÇÀû ÃÑÇÕ
+	// ï¿½Ð±âº° ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
 	int sum_one = kim[0] + lee[0] + park[0] + song[0] + choi[0];
 	int sum_two = kim[1] + lee[1] + park[1] + song[1] + choi[1];
 	int sum_three = kim[2] + lee[2] + park[2] + song[2] + choi[2];
 	int sum_four = kim[3] + lee[3] + park[3] + song[3] + choi[3];
 
-	// ÇÑ ÇØ °³ÀÎ ½ÇÀû ÃÑÇÕ
+	// ï¿½ï¿½ ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
 	int sum_year = kim[4] + lee[4] + park[4] + song[4] + choi[4];
 
-	// ºÐ±âº° °³ÀÎ ½ÇÀû Æò±Õ
+	// ï¿½Ð±âº° ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½
 	double avg_one = sum_one / 5;
 	double avg_two = sum_two / 5;
 	double avg_three = sum_three / 5;
 	double avg_four = sum_four / 5;
 	
-	// ÇÑ ÇØ °³ÀÎ ½ÇÀû Æò±Õ
+	// ï¿½ï¿½ ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½
 	double avg_year = sum_year / 5;
 
-	// ±×·¡ÇÁ
+	// ï¿½×·ï¿½ï¿½ï¿½
 	int graph_one = ((avg_one) * 100 / avg_year) / 10 ;
 	int graph_two = ((avg_two) * 100 / avg_year) / 10 ;
 	int graph_three = ((avg_three) * 100 / avg_year) / 10;
@@ -34,13 +34,13 @@ int main(void) {
 
 	int graph[5] = { graph_one, graph_two, graph_three, graph_four };
 
-	printf("1ºÐ±â ÃÑÇÕ : %d\n", sum_one);
-	printf("1ºÐ±â Æò±Õ : %.2f\n", avg_one);
-	printf("1³â Æò±Õ : %.2f\n", avg_year);
-	printf("1ºÐ±â ±×·¡ÇÁ : %d\n", graph_one);
-	printf("2ºÐ±â ±×·¡ÇÁ : %d\n", graph_two);
-	printf("3ºÐ±â ±×·¡ÇÁ : %d\n", graph_three);
-	printf("4ºÐ±â ±×·¡ÇÁ : %d\n", graph_four);
+	printf("1ï¿½Ð±ï¿½ ï¿½ï¿½ï¿½ï¿½ : %d\n", sum_one);
+	printf("1ï¿½Ð±ï¿½ ï¿½ï¿½ï¿½ : %.2f\n", avg_one);
+	printf("1ï¿½ï¿½ ï¿½ï¿½ï¿½ : %.2f\n", avg_year);
+	printf("1ï¿½Ð±ï¿½ ï¿½×·ï¿½ï¿½ï¿½ : %d\n", graph_one);
+	printf("2ï¿½Ð±ï¿½ ï¿½×·ï¿½ï¿½ï¿½ : %d\n", graph_two);
+	printf("3ï¿½Ð±ï¿½ ï¿½×·ï¿½ï¿½ï¿½ : %d\n", graph_three);
+	printf("4ï¿½Ð±ï¿½ ï¿½×·ï¿½ï¿½ï¿½ : %d\n", graph_four);
 
 	int i, j, k;
 	
