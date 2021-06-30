8dce82d20073086037728b5a68b25580ed7d36fb
skanwngud <skanwngud@gmail.com>
2021-06-30 22:56:13 +0900

    commit

--- StudyC/pointer_real/Study_Self.c
+++ StudyC/pointer_real/Study_Self.c
@@ -8,7 +8,7 @@ int main(void)
 	double max, mid, min;
 
 	printf("실수값 3개 입력 : ");
-	scanf("%lf%lf%lf", &max, &mid, &min);
+	scanf("%lf%lf%lf", &max, &mid, &min); // 2.5 1.5 3.5
 
 	line_up(&max, &mid, &min);
 	printf("정렬 된 값 출력 : %.1lf, %.1lf, %.1lf", max, mid, min);
@@ -25,16 +25,16 @@ void swap(double* pa, double* pb)
 	*pb = temp;
 }
 
-void line_up(double* maxp, double* midp, double* minp)
+void line_up(double* maxp, double* midp, double* minp) // 2.5 1.5 3.5
 {
-	double temp;
-
-	if (maxp < midp && midp < minp) {
-		swap(maxp, midp);
-		swap(midp, minp);
+	if (*maxp < *midp) { // 2.5 < 1.5 성립 안 되므로 넘어감
+		swap(maxp, midp); // max 2.5 mid 1.5
 	}
-	else if (maxp < minp) {
-		swap(maxp, minp);
+	if (*maxp < *minp) { // 2.5 < 3.5 성립 되므로 바뀜
+		swap(maxp, minp); // max 3.5 min 2.5
+	}
+	if (*midp < *minp) {
+		swap(midp, minp);
 	}
 
 }
